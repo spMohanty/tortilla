afa80901d0912416c5d2e1b367191e0ed69228b8
S.P. Mohanty <spmohanty91@gmail.com>
2018-02-02 18:52:49 +0100

    Organise data loaders

---
+++ data_loaders.py
@@ -0,0 +1,40 @@
+import torch
+from torchvision import datasets, models, transforms
+
+from config import Config as config
+
+"""
+Define transforms
+"""
+
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomSizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomVerticalFlip(),
+        transforms.RandomRotation(180),
+        transforms.ColorJitter(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Scale(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
+
+"""
+Define dataloaders
+"""
+
+train_dataset = datasets.ImageFolder(
+                    config.data_dir,
+                    data_transforms["train"],
+                    )
+train_data_loader = torch.utils.data.DataLoader(
+                train_dataset,
+                32,
+                True,
+                num_workers=2)
--- main.py
+++ main.py
@@ -4,41 +4,7 @@ import torch
 from torchvision import datasets, models, transforms
 
 from config import Config as config
-
-"""
-Define transforms
-"""
-
-data_transforms = {
-    'train': transforms.Compose([
-        transforms.RandomSizedCrop(224),
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomVerticalFlip(),
-        transforms.RandomRotation(180),
-        transforms.ColorJitter(),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-    'val': transforms.Compose([
-        transforms.Scale(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-}
-
-"""
-Define dataloaders
-"""
-train_dataset = datasets.ImageFolder(
-                    config.data_dir,
-                    data_transforms["train"],
-                    )
-train_data_loader=torch.utils.data.DataLoader(
-                train_dataset,
-                32,
-                True,
-                num_workers=2)
+from data_loaders import train_data_loader
 
 for images, labels in train_data_loader:
     print(images.shape)
