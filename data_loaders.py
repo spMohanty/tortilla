c1caba74f8598f19807d420e5515a634ffb7aaa9
S.P. Mohanty <spmohanty91@gmail.com>
2018-02-06 10:53:28 +0100

    Consolidate TortillaDataset class

--- data_loaders.py
+++ data_loaders.py
@@ -47,38 +47,78 @@ class ImageFilelist(data.Dataset):
 	def __len__(self):
 		return len(self.imlist)
 
-"""
-Define transforms
-"""
+class TortillaDataset:
+	def __init__(self, dataset_folder, data_transforms=None,
+				shuffle=True, batch_size=32, num_cpu_workers=4):
+		self.dataset_folder = dataset_folder
+		self.data_transforms = data_transforms
+		self.shuffle = shuffle
+		self.batch_size = batch_size
+		self.num_cpu_workers = num_cpu_workers
 
-data_transforms = {
-    'train': transforms.Compose([
-        transforms.RandomSizedCrop(224),
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomVerticalFlip(),
-        transforms.RandomRotation(180),
-        transforms.ColorJitter(),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-    'val': transforms.Compose([
-        transforms.Scale(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-}
+		self.classes = open(os.path.join(self.dataset_folder,
+										"classes.txt")).readlines()
+		"""
+		Define transforms
+		"""
+		if data_transforms == None:
+			self.data_transforms = {
+			    'train': transforms.Compose([
+			        transforms.RandomSizedCrop(224),
+			        transforms.RandomHorizontalFlip(),
+			        transforms.RandomVerticalFlip(),
+			        transforms.RandomRotation(180),
+			        transforms.ColorJitter(),
+			        transforms.ToTensor(),
+			        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+			    ]),
+			    'val': transforms.Compose([
+			        transforms.Scale(256),
+			        transforms.CenterCrop(224),
+			        transforms.ToTensor(),
+			        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+			    ]),
+			}
+		"""
+			Define datasets from filelists
+		"""
+		train_filelist = os.path.join(self.dataset_folder, "train.txt")
+		val_filelist = os.path.join(self.dataset_folder, "val.txt")
+		self.train_dataset = ImageFilelist(
+								self.dataset_folder,
+								train_filelist,
+								transform=self.data_transforms["train"]
+								)
+		self.val_dataset = ImageFilelist(
+								self.dataset_folder,
+								val_filelist,
+								transform=self.data_transforms["val"]
+								)
 
-"""
-Define dataloaders
-"""
+		"""
+		Define dataloaders
+		"""
+		self.train_data_loader = torch.utils.data.DataLoader(
+				dataset=self.train_dataset,
+				batch_size=self.batch_size,
+				shuffle=self.shuffle,
+				num_workers=self.num_cpu_workers
+		)
+		self.train = self.train_data_loader
 
-train_dataset = datasets.ImageFolder(
-                    config.data_dir,
-                    data_transforms["train"],
-                    )
-train_data_loader = torch.utils.data.DataLoader(
-                train_dataset,
-                config.batch_size,
-                True,
-                num_workers=10)
+		self.val_data_loader = torch.utils.data.DataLoader(
+				dataset=self.val_dataset,
+				batch_size=self.batch_size,
+				shuffle=self.shuffle,
+				num_workers=self.num_cpu_workers
+		)
+		self.val = self.val_data_loader
+
+if __name__ == "__main__":
+	dataset = TortillaDataset(	"datasets/food-101",
+								batch_size=128,
+								num_cpu_workers=10
+								)
+	for (images, labels) in dataset.train:
+		print(images.shape, labels.shape)
+		exit(0)
