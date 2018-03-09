b83b0d3fd994301bb391b23f8eb5bdfa33cf4b7b
ÃCamille Renner <camille.renner@epfl.ch>
2018-03-09 14:28:57 +0100

    Draft changes for refactoring prepare_data

--- scripts/data-prepartion/utils.py
+++ scripts/data-prepartion/utils.py
@@ -2,6 +2,7 @@ import sys
 import os
 import shutil
 import glob
+from PIL import Image
 
 def files_in_a_directory(folder_path):
     files = glob.glob(folder_path+"/*")
@@ -40,7 +41,7 @@ def output_folder_path_validation(output_folder_path, classes):
             shutil.rmtree(output_folder_path)
         else:
             print("Exiting, because output folder path exists \
-                  and cannot be deleted.")
+            and cannot be deleted.")
             exit(0)
 
     """
@@ -63,21 +64,70 @@ def output_folder_path_validation(output_folder_path, classes):
 
 def get_classes_from_input_folder(input_folder_path):
     dot_files = glob.glob(input_folder_path+"/.*")
-    dot_files = [x.replace(input_folder_path+"/","") for x in dot_files]
-    classes = set(os.listdir(input_folder_path)) - set(dot_files)
+    dot_files = [x.replace(input_folder_path,"") for x in dot_files]
+    folders_list = set(os.listdir(input_folder_path)) - set(dot_files)
+    print(folders_list)
     final_classes = []
-    for _class in classes:
-        if os.path.isdir(os.path.join(input_folder_path, _class)):
-            final_classes.append(_class)
+    for _folder in folders_list:
+        if os.path.isdir(os.path.join(input_folder_path, _folder)): #Only keep folders
+            files_list = set(os.listdir(os.path.join(input_folder_path, _folder)))
+            if files_list: #Discard empty folders
+                print("Full folder: {} ".format(_folder))
+                print(list(files_list)[:10])
+                stop = False;
+                print(len(files_list))
+                while not stop:
+                    for _idx, _file in enumerate(files_list):
+                        print("Processing {}/{} :: {}".format(str(_idx), str(len(files_list)), _file))
+                        try:
+                            print("Try")
+                            im=Image.open(os.path.join(input_folder_path, _folder,_file))
+                            print("Yes")
+                            stop = True;
+                            final_classes.append(_folder)
+                            print("Class added.")
+                            break
+                        except:
+                            print("Except")
+                            if _idx == len(files_list)-1:
+                                print("Warning: the folder {} does not contain valid images.".format(_folder))
+                                response = query_yes_no(
+                                            "Do you want to continue ?",
+                                            default='no')
+
+                                if not response:
+                                    print("Exiting.")
+                                    exit(0)
+                                stop = True;
+                            else:
+                                continue
+                #if one_valid_image:
+                    #break
+                #for _file in sub_folders_list:
+                # need editing for valid image check
+                # either while true try Image or check for .jpg...
+                # final_classes.append(_folder)
+    if not final_classes:
+        print("Exiting because none of the subfolders contains valid images.")
+        exit(0)
+    print(final_classes)
     return final_classes
 
 def input_folder_path_validation(input_folder_path):
     """
         Validation of the Input folder path
+
+        If the folder is not a directory, then the program exits.
     """
-    classes = get_classes_from_input_folder(input_folder_path)
-    for _class in classes:
-        assert(os.path.isdir(os.path.join(input_folder_path, _class)))
+    if os.path.isdir(input_folder_path):
+        classes = get_classes_from_input_folder(input_folder_path)
+        #I don't know if this is really useful because already done in get_classes
+        for _class in classes:
+            assert(os.path.isdir(os.path.join(input_folder_path, _class)))
+    else:
+        print("Exiting because Input Folder Path is not a directory, \
+                please provide a folder containing sub-folders of images")
+        exit(0)
 
 def query_yes_no(question, default="yes"):
     """Ask a yes/no question via raw_input() and return their answer.
