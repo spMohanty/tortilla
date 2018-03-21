6f2bc30c71dcde998abf8a1cc8b5c12c418ead31
ÃCamille Renner <camille.renner@epfl.ch>
2018-03-21 15:40:01 +0100

    move all tests into tests folder â#28

--- tests/test_me.py
+++ tests/test_me.py
@@ -1,6 +1,5 @@
 from nose.tools import assert_raises
 import numpy as np
-import json
 from scripts.data_preparation.utils import *
 
 class TestClass:
@@ -20,22 +19,10 @@ class TestClass:
             get_classes_from_input_folder('tests/data/no-valid-image-folder',non_interactive_mode=True)
         assert cm.exception.args[0] == 'No Valid Images'
 
-    def test_min_images_exit(self):
-        classes = ['c_0', 'c_1', 'c_2', 'c_3', 'c_5']
-        with assert_raises(SystemExit) as cm:
-            final_classes = min_images_validation('tests/data/plant_diseases', classes, min_images_per_class=275)
-        assert cm.exception.args[0] == 'Not Enough Images'
-
     def test_classes_creation(self):
         classes = get_classes_from_input_folder('tests/data/plant_diseases', non_interactive_mode=True)
         assert len(classes) == 5
         assert set(classes) == set(np.array(['c_0', 'c_1', 'c_2', 'c_3', 'c_5']))
-
-    def test_min_images(self):
-        classes = ['c_0', 'c_1', 'c_2', 'c_3', 'c_5']
-        final_classes = min_images_validation('tests/data/plant_diseases', classes, min_images_per_class=200)
-        assert len(final_classes) == 2
-        assert set(final_classes) == set(np.array(['c_0', 'c_1']))
 
     def test_output_folder_creation(self):
         test_classes = np.array(['A', 'B', 'C'])
@@ -52,7 +39,7 @@ class TestClass:
             output_folder_path_validation('tests/test2', test_classes_2, non_interactive_mode=True)
         assert cm.exception.args[0] == 'No deletion of Output Folder'
         shutil.rmtree('tests/test2')
-  
+
     def test_prepare_data(self):
         result = os.system("python scripts/data_preparation/prepare_data.py --input-folder-path=tests/data/plant_diseases/ --output-folder-path=tests/test3/ --dataset-name=test3 --min-images-per-class=50 --max-images-per-class=100 --non_interactive_mode=True")
         assert os.path.exists('tests/test3') == True
