0c0f6198da5484063c4e8bb77db4aedba265e665
S.P. Mohanty <spmohanty91@gmail.com>
2018-03-22 13:42:41 +0100

    Add a main function

--- scripts/misc/prepare_datasets_recursive.py
+++ scripts/misc/prepare_datasets_recursive.py
@@ -33,31 +33,34 @@ def sanitise_class_name(className):
     className = className.replace("/","::").replace(" ", "_")
     return className
 
-for dirName, subdirList, fileList in os.walk(SOURCE_IMAGES):
-    if len(subdirList) == 0:
-        """
-        THis is a leaf node, no need to create a dataset for this
-        """
-    elif len(subdirList) == 1:
-        """
-        This is not a leaf node, but still has just 1 sub-class,
-        and we need atleast two subclasses to create a dataset
-        """
-    else:
-        """
-        Case when this directory has more than one sub-classes
-        """
-        CLASSES = []
-        FILES = []
-        DATASET_NAME = sanitise_class_name(dirName)
+def main(SOURCE_IMAGES):
+    for dirName, subdirList, fileList in os.walk(SOURCE_IMAGES):
+        if len(subdirList) == 0:
+            """
+            THis is a leaf node, no need to create a dataset for this
+            """
+        elif len(subdirList) == 1:
+            """
+            This is not a leaf node, but still has just 1 sub-class,
+            and we need atleast two subclasses to create a dataset
+            """
+        else:
+            """
+            Case when this directory has more than one sub-classes
+            """
+            CLASSES = []
+            FILES = []
+            DATASET_NAME = sanitise_class_name(dirName)
 
-        for subDir in subdirList:
-            subDirPath = os.path.join(dirName, subDir)
-            className = os.path.join(dirName, subDir)
-            className = sanitise_class_name(className)
+            for subDir in subdirList:
+                subDirPath = os.path.join(dirName, subDir)
+                className = os.path.join(dirName, subDir)
+                className = sanitise_class_name(className)
 
-            all_files = all_valid_files(subDirPath, is_valid)
-            CLASSES.append(className)
-            FILES.append(all_files)
+                all_files = all_valid_files(subDirPath, is_valid)
+                CLASSES.append(className)
+                FILES.append(all_files)
 
-        print(DATASET_NAME, CLASSES, [len(x) for x in FILES])
+            print(DATASET_NAME, CLASSES, [len(x) for x in FILES])
+
+main(SOURCE_IMAGES)
