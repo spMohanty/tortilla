5ac30975b577a6a0b642798ab23406df7651cee6
ÃCamille Renner <camille.renner@epfl.ch>
2018-03-23 17:08:08 +0100

    Add CLI arguments

---
+++ scripts/misc/create_recursive_datasets.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir output
+mkdir my_food_repo
+python scripts/misc/prepare_datasets_recursive.py --input-folder-path=/mount/SDE/instagram/myfoodrepo-images/myfoodrepo-images/images --output-folder-path=output
+cd output
+for f in *
+  do
+    python ../scripts/data_preparation/prepare_data.py --input-folder-path=$f/ --output-folder-path=../my_food_repo/$f/ --dataset-name=$f --non_interactive_mode=True --min-images-per-class=100
+done
